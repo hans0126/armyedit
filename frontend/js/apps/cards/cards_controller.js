a1cb05e5d7940426a26d6a9118812a849efb1274
hans0126 <hayashi0126@gmail.com>
2015-10-05 18:14:17 +0800

    img thumb

--- frontend/js/apps/cards/cards_controller.js
+++ frontend/js/apps/cards/cards_controller.js
@@ -1,7 +1,7 @@
 define(function(require) {
 
     var app = require("app");
-
+    require('js/lib/thumbEdit/thumbEdit.js');
     app.controller("cards_controller", ["$scope",
         "search",
         "statusAvgService",
@@ -15,10 +15,11 @@ define(function(require) {
 
             _self.c = getCategoryService;
 
+
+
             search.itemSelect = function(_obj) {
                 _self.editCard.getDuplicate(_obj);
             }
-
 
             _self.editCard = {
                 fa: [{
@@ -30,19 +31,10 @@ define(function(require) {
                 }, {
                     title: "Number",
                     brief: "n",
-                    num:null
+                    num: null
                 }],
                 cardTemplate: {
-                    title: null,
-                    status: {
-                        spd: null,
-                        str: null,
-                        mat: null,
-                        rat: null,
-                        def: null,
-                        arm: null,
-                        cmd: null
-                    },
+                    title: "A",
                     img: {
                         normal: null,
                         thumb: null
@@ -53,27 +45,121 @@ define(function(require) {
                     pc: null,
                     fa: null,
                     wj: null,
-                    focus: null
+                    group: null,
+                    actor: []
+
+                },
+                actorTemplate: {
+                    idx:0,
+                    title: 1,
+                    status: {
+                        spd: 2,
+                        str: 3,
+                        mat: 4,
+                        rat: 5,
+                        def: 6,
+                        arm: 7,
+                        cmd: null,
+                        focus: null
+                    },
+                    img: {
+                        thumb: null
+                    }
                 },
                 getDuplicate: function(_obj) {
                     this.reset();
-                    this.primaryCard.title = "A";
+                    // this.primaryCard.title = "A";
                 },
                 primaryCard: null,
                 reset: function() {
-                    this.primaryCard = angular.copy(this.cardTemplate)
+                    this.primaryCard = angular.copy(this.cardTemplate);
+                    this.primaryCard.actor.push(angular.copy(this.actorTemplate));
+                },
+                init: function() {
+                    this.reset();
+                },
+                addActor:function(){
+                     this.primaryCard.actor.push(angular.copy(this.actorTemplate));                     
+                },
+                removeActor:function(key){                  
+                    var _actor = this.primaryCard.actor;
+                    for(var i=0;i<_actor.length;i++){
+                        if(_actor[i].$$hashkey==key){
+                           _actor.splice(i,1);
+                           break; 
+                        }
+                    }
                 }
+
 
             }
 
 
+            _self.editCard.init();
+
             _self.statusAvgService = statusAvgService;
 
+            _self.thumbShow = false;
 
+            //$scope.thumbImg = null;
 
         }
     ])
 
+
+    app.directive('customOnChange', function() {
+        return {
+            restrict: 'A',
+            scope: {
+                thumb: "@",
+                thumbs: "@",
+                showThumb: "=",
+                thumbImg: "="
+            },
+            link: function(scope, element, attrs) {
+                // get function and bind change Event
+                var onChangeFunc = scope.$eval(attrs.customOnChange);
+                element.bind('change', onChangeFunc);
+            },
+            controller: function($scope) {
+                // tips : define function in controller  , directive use this function 
+                var _t = new thumbEdit();
+                $scope.uploadFile = function() {
+                    $scope.showThumb = true;
+
+                    var filename = event.target.files[0].name;
+                    var reader = new FileReader();
+                    reader.onload = function(e) {
+                        $scope.thumbImg = e.target.result;
+                        $scope.$apply();
+                        _t.init($scope.thumb, e.target.result, $scope.thumbs);
+                    };
+                    reader.readAsDataURL(event.target.files[0]);
+                };
+
+            }
+        };
+    });
+
+    app.directive("thumbImages", function() {
+        return {
+            restrict: 'A',
+            scope: {
+                thumbImg: "=thumbImages",
+                idx: "@"
+            },
+            link: function(scope, element, attr) {
+                var _b = element.find('button');
+                _b.bind('click', function() {
+                    var _t = new thumbEdit();
+                    _t.init('thumb'+scope.idx, scope.thumbImg, 'thumbs'+scope.idx);
+                })
+
+                //  console.log(scope.thumbImg)
+            }
+
+        }
+    })
 
 
 
--- frontend/js/lib/thumbEdit/thumbEdit.js
+++ frontend/js/lib/thumbEdit/thumbEdit.js
@@ -1,192 +1,197 @@
    function thumbEdit() {
-        var _self = this;
-        _self.canvasElement = null;
-        _self.canvasWidth = 320;
-        _self.canvasHeight = 320;
-        _self.crop = {
-            w: 100,
-            h: 100
-        }
+       var _self = this;
+       _self.canvasElement = null;
+       _self.canvasWidth = 320;
+       _self.canvasHeight = 320;
+       _self.crop = {
+           w: 100,
+           h: 100
+       }
 
-        var imgPos = {
-            x: 0,
-            y: 0
-        }
+       var imgPos = {
+           x: 0,
+           y: 0
+       }
 
-        _self.output = {
-            x: 0,
-            y: 0
-        }
+       _self.output = {
+           x: 0,
+           y: 0
+       }
 
-        var pos = {
-            ox: 0,
-            oy: 0,
-            dx: 0,
-            dy: 0
-        }
+       var pos = {
+           ox: 0,
+           oy: 0,
+           dx: 0,
+           dy: 0
+       }
 
-        var actors = [];
+       var actors = [];
 
-        var imageObj = new Image();
-        var outputELement = null; //use jquery
+       var imageObj = new Image();
+       var outputELement = null; //use jquery
 
-        _self.loadData = function(_x, _y) {
-            var _baseX = _self.canvasWidth / 2 - _self.crop.w / 2;
-            var _baseY = _self.canvasHeight / 2 - _self.crop.h / 2;
+       _self.loadData = function(_x, _y) {
+           var _baseX = _self.canvasWidth / 2 - _self.crop.w / 2;
+           var _baseY = _self.canvasHeight / 2 - _self.crop.h / 2;
 
-            imgPos.x = _baseX + _x;
-            imgPos.y = _baseY + _y;
+           imgPos.x = _baseX + _x;
+           imgPos.y = _baseY + _y;
 
-            _self.output.x = _x;
-            _self.output.y = _y
+           _self.output.x = _x;
+           _self.output.y = _y
 
-        }
+       }
 
-        _self.init = function(_elementId, _imgUrl, _outputElement) {
-            _self.canvasElement = document.getElementById(_elementId);
-            _self.canvasElement.width = _self.canvasWidth;
-            _self.canvasElement.height = _self.canvasHeight;
+       _self.init = function(_elementId, _imgUrl, _outputElement) {
+           _self.canvasElement = document.getElementById(_elementId);
+           _self.canvasElement.width = _self.canvasWidth;
+           _self.canvasElement.height = _self.canvasHeight;
 
-            //change cursor
-            var style = document.createElement('style');
-            var cssText = "#" + _elementId + ":active {cursor: crosshair;}";
-            style.appendChild(document.createTextNode(cssText));
-            document.getElementsByTagName('head')[0].appendChild(style);
-
-
-            outputELement = _outputElement;
-            imageObj.src = _imgUrl;
-
-            outputELement.css({
-                "display": "block",
-                "width": "100px",
-                "height": "100px",
-                "border": "1px solid #333",
-                "background-repeat": "no-repeat",
-                "background-image": "url(" + imageObj.src + ")"
-            })
-
-            thumbRender();
-
-            drawStart();
-
-            _self.canvasElement.addEventListener('mousedown', function(evt) {
-
-                var mousePos = getMousePos(_self.canvasElement, evt);
-                pos.ox = mousePos.x;
-                pos.oy = mousePos.y;
-
-                _self.canvasElement.addEventListener('mousemove', moveEvent);
-
-            });
-
-            _self.canvasElement.addEventListener('mouseup', function(evt) {
-
-                _self.output.x = (imgPos.x - (_self.canvasWidth / 2) + _self.crop.w / 2);
-                _self.output.y = (imgPos.y - (_self.canvasHeight / 2) + _self.crop.h / 2);
-
-                _self.canvasElement.removeEventListener('mousemove', moveEvent);
+           //change cursor
+           var style = document.createElement('style');
+           var cssText = "#" + _elementId + ":active {cursor: crosshair;}";
+           style.appendChild(document.createTextNode(cssText));
+           document.getElementsByTagName('head')[0].appendChild(style);
 
 
-                thumbRender();
+           _self.canvasElement.style.border = "1px solid #333";
 
-            })
-        }
+           // outputELement = _outputElement;
+           outputELement = document.getElementById(_outputElement);
+           imageObj.src = _imgUrl;
+
+           var thumbCss = {
+               "display": "block",
+               "width": _self.crop.w + "px",
+               "height": _self.crop.h + "px",
+               "border": "1px solid #333",
+               "backgroundRepeat": "no-repeat",
+               "backgroundImage": "url(" + imageObj.src + ")"
+           }
+
+           for (var key in thumbCss) {
+               outputELement.style[key] = thumbCss[key];
+           }
+
+           thumbRender();
+
+           drawStart();
+
+           _self.canvasElement.addEventListener('mousedown', function(evt) {
+
+               var mousePos = getMousePos(_self.canvasElement, evt);
+               pos.ox = mousePos.x;
+               pos.oy = mousePos.y;
+
+               _self.canvasElement.addEventListener('mousemove', moveEvent);
+
+           });
+
+           _self.canvasElement.addEventListener('mouseup', function(evt) {
+
+               _self.output.x = (imgPos.x - (_self.canvasWidth / 2) + _self.crop.w / 2);
+               _self.output.y = (imgPos.y - (_self.canvasHeight / 2) + _self.crop.h / 2);
+
+               _self.canvasElement.removeEventListener('mousemove', moveEvent);
+
+
+               thumbRender();
+
+           })
+       }
 
 
 
-        imageObj.onload = function() {
+       imageObj.onload = function() {
 
-            var _img = new actor();
-            _img.zIndex = 10;
-            _img.ctx = _self.canvasElement.getContext("2d");
-            _img.draw = function() {
-                if (this.renew) {
-                    this.clearRect();
-                }
+           var _img = new actor();
+           _img.zIndex = 10;
+           _img.ctx = _self.canvasElement.getContext("2d");
+           _img.draw = function() {
+               if (this.renew) {
+                   this.clearRect();
+               }
 
-                imgPos.x += pos.dx;
-                imgPos.y += pos.dy;
+               imgPos.x += pos.dx;
+               imgPos.y += pos.dy;
 
-                this.ctx.drawImage(imageObj, imgPos.x, imgPos.y);
-            }
+               this.ctx.drawImage(imageObj, imgPos.x, imgPos.y);
+           }
 
-            actors.push(_img);
+           actors.push(_img);
 
-            actors.sort(function(a, b) {
-                return a.zIndex - b.zIndex;
-            })
+           actors.sort(function(a, b) {
+               return a.zIndex - b.zIndex;
+           })
 
-            render();
-        };
+           render();
+       };
 
-        function drawStart() {
+       function drawStart() {
 
-            var cropRect = new actor();
-            cropRect.zIndex = 100;
-            cropRect.ctx = _self.canvasElement.getContext("2d");
-            cropRect.renew = false;
-            cropRect.draw = function() {
+           var cropRect = new actor();
+           cropRect.zIndex = 100;
+           cropRect.ctx = _self.canvasElement.getContext("2d");
+           cropRect.renew = false;
+           cropRect.draw = function() {
 
-                var _posX = _self.canvasWidth / 2 - _self.crop.w / 2,
-                    _posY = _self.canvasHeight / 2 - _self.crop.h / 2;
+               var _posX = _self.canvasWidth / 2 - _self.crop.w / 2,
+                   _posY = _self.canvasHeight / 2 - _self.crop.h / 2;
 
-                if (this.renew) {
-                    this.clearRect()
-                }
+               if (this.renew) {
+                   this.clearRect()
+               }
 
-                this.ctx.strokeStyle = "#333333";
-                this.ctx.lineWidth = 1;
-                this.ctx.strokeRect(_posX, _posY, _self.crop.w, _self.crop.h);
-            }
+               this.ctx.strokeStyle = "#333333";
+               this.ctx.lineWidth = 1;
+               this.ctx.strokeRect(_posX, _posY, _self.crop.w, _self.crop.h);
+           }
 
-            actors.push(cropRect);
+           actors.push(cropRect);
 
-        }
+       }
 
-        function actor() {
-            this.zIndex = 0;
-            this.draw = function() {};
-            this.ctx = {};
-            this.renew = true;
-            this.clearRect = function() {
-                this.ctx.clearRect(0, 0, _self.canvasWidth, _self.canvasHeight);
-                this.ctx.beginPath();
-            }
-        }
+       function actor() {
+           this.zIndex = 0;
+           this.draw = function() {};
+           this.ctx = {};
+           this.renew = true;
+           this.clearRect = function() {
+               this.ctx.clearRect(0, 0, _self.canvasWidth, _self.canvasHeight);
+               this.ctx.beginPath();
+           }
+       }
 
-        function getMousePos(canvas, evt) {
-            var rect = canvas.getBoundingClientRect();
+       function getMousePos(canvas, evt) {
+           var rect = canvas.getBoundingClientRect();
 
-            return {
-                x: evt.clientX - rect.left,
-                y: evt.clientY - rect.top
-            };
-        }
+           return {
+               x: evt.clientX - rect.left,
+               y: evt.clientY - rect.top
+           };
+       }
 
-        function moveEvent(evt) {
+       function moveEvent(evt) {
 
-            var mousePos = getMousePos(_self.canvasElement, evt);
+           var mousePos = getMousePos(_self.canvasElement, evt);
 
-            pos.dx = mousePos.x - pos.ox;
-            pos.dy = mousePos.y - pos.oy;
+           pos.dx = mousePos.x - pos.ox;
+           pos.dy = mousePos.y - pos.oy;
 
-            render();
+           render();
 
-            pos.ox = mousePos.x;
-            pos.oy = mousePos.y;
-        }
+           pos.ox = mousePos.x;
+           pos.oy = mousePos.y;
+       }
 
-        function render() {
-            actors.forEach(function(doc) {
-                doc.draw();
-            })
-        }
+       function render() {
+           actors.forEach(function(doc) {
+               doc.draw();
+           })
+       }
 
-        function thumbRender() {
-            outputELement.css({
-                "background-position": _self.output.x + "px " + _self.output.y + "px"
-            })
-        }
+       function thumbRender() {
+           outputELement.style.backgroundPosition = _self.output.x + "px " + _self.output.y + "px";
+       }
 
-    }
+   }
