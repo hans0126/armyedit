8c96cc58f6c1e2df33a45d1d37ed70c32c6a8137
hans0126 <hayashi0126@gmail.com>
2015-11-01 15:15:18 +0800

    lightbox prototype success

--- frontend/js/apps/main_controller.js
+++ frontend/js/apps/main_controller.js
@@ -7,42 +7,47 @@ define(function(require) {
     app.controller("mainCtrl", ["$scope",
         "$location",
         "msgService",
-        "settingService",
-        "ll",
-        function($scope, $location, msgService, setting, ll) {
+        "settingService",    
+        function($scope, $location, msgService, setting) {
 
             var _self = this;
 
-            setting.init();
-
-            _self.ss = ll;
-
-            _self.cc = function() {
-                // event.preventDefault();
-                console.log('Clicked!');
-                ll.show = false;
-                var body = document.getElementsByTagName('html');
-                body = angular.element(body);
-                 body.removeClass('lightBoxFixed');
-            }
-
-            //   console.log(ll.show);
-
-            // _self.setting = setting;
+            setting.init();        
 
             _self.go = function(path) {
                 $location.path(path);
             }
             $scope.msg = msgService;
-
-
         }
     ])
 
 
+    app.service('lightBoxService', ['$compile', function($compile) {
+        var _self = this;
+        _self.visible = true;
+        _self.htmlBody = angular.element(document.getElementsByTagName('html'));
+        var lightBox = angular.element(document.getElementById('lightBox'));
+
+        _self.close = function(fn) {
+            _self.visible = false;
+            _self.htmlBody.removeClass('lightBoxFixed');
+            if (typeof(fn) == 'function') {
+                fn();
+            }
+
+            lightBox.empty();
+        }
+
+        _self.open = function(compiledObj) {
+            _self.visible = false;
+            _self.htmlBody.addClass('lightBoxFixed');
+            if (compiledObj) {
+                lightBox.append(compiledObj);
+            }
+        }
+    }])
 
     app.service('ll', function() {
-
         this.show = false;
     })
 
@@ -59,7 +64,6 @@ define(function(require) {
         _self.icon = null;
         _self.showMsg = function(_text, _typeIndex) {
 
-
             _self.msgType = _type[_typeIndex];
             _self.msgText = _text;
             _self.icon = _icon[_typeIndex];
@@ -72,10 +76,21 @@ define(function(require) {
                 _self.show = null;
 
             }, 3000)
-
-
         }
     })
+
+
+    app.directive("lightbox", ['lightBoxService', function(lightBoxService) {
+        return {
+            replace: true,
+            restrict: "A",
+            link: function(scope, element, atrr) {
+                scope.lightbox = lightBoxService;
+            },
+            template: "<div id='lightBox' ng-if='lightbox.visible'><div class='overlay' ng-click='lightbox.close()'></div><div class='popup'>content</div></div>"
+
+        }
+    }])
 
 
     app.directive("msg", function($rootScope) {
